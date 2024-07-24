4105ac696bc1f2668156431826e229b3124e761c
rohansh-tty <rohanshetty.dev@gmail.com>
2024-07-25 00:38:59 +0530

    chore: added dump and restore api

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"archive/zip"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"database/sql"
 	"encoding/base64"
+	"encoding/csv"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	mathrand "math/rand"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -25,6 +29,120 @@ const (
 	keySize         = 32 // AES-256
 	fixedCipherSize = 64
 )
+
+func dumpPasswordsToZip(db *sql.DB, filename string) error {
+	// create zip file
+	zipFile, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create a zip file, %v", err)
+	}
+	defer zipFile.Close() // to close file in the end
+
+	// zipWriter init
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	// create csv file inside zip
+	csvFile, err := zipWriter.Create("passwords.csv")
+	if err != nil {
+		return fmt.Errorf("failed to create passwords.csv file, %v", err)
+	}
+
+	// csvWriter init
+	csvWriter := csv.NewWriter(csvFile)
+	defer csvWriter.Flush()
+
+	// write CSV Header
+	if err := csvWriter.Write([]string{"app_name", "username", "password", "created_at"}); err != nil {
+		return fmt.Errorf("failed to write csv header, %v", err)
+	}
+
+	// fetch data from db
+	rows, err := db.Query("SELECT app_name, username, password, created_at FROM passwords")
+	if err != nil {
+		return fmt.Errorf("failed to fetch data from Database, %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var appName, userName, password, createdAt string
+		if err := rows.Scan(&appName, &userName, &password, &createdAt); err != nil {
+			return fmt.Errorf("failed to assign data from row, %v", err)
+		}
+
+		if err := csvWriter.Write([]string{appName, userName, password, createdAt}); err != nil {
+			return fmt.Errorf("failed to write data to csv, %v", err)
+		}
+
+	}
+	return nil
+}
+
+func restorePasswordsToZip(db *sql.DB, filename string) error {
+	// open zip file
+	reader, err := zip.OpenReader(filename)
+	if err != nil {
+		return fmt.Errorf("failed to read zip file, %v", err)
+	}
+	defer reader.Close()
+
+	var csvFile *zip.File
+	for _, file := range reader.File {
+		if filepath.Ext(file.Name) == ".csv" {
+			csvFile = file
+			break
+		}
+	}
+	if csvFile == nil {
+		return fmt.Errorf("no .csv file found in zip")
+	}
+	// open csv file
+	fileReader, err := csvFile.Open()
+	if err != nil {
+		return fmt.Errorf("failed to open csv file, %v", err)
+	}
+	defer fileReader.Close()
+
+	// csv reader
+	csvReader := csv.NewReader(fileReader)
+
+	// skipping header row
+	if _, err := csvReader.Read(); err != nil {
+		return fmt.Errorf("failed to read csv header, %v", err)
+	}
+
+	// create empty passwords table, if not exists
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS passwords2 (
+			app_name TEXT NOT NULL,
+			username TEXT NOT NULL,
+			password TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+	if err != nil {
+		return fmt.Errorf("failed to create table, %v", err)
+	}
+
+	// write row data into db
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read csv record, %v", err)
+		}
+
+		// write into db
+		_, dbErr := db.Exec("INSERT INTO passwords2 (app_name, username, password, created_at) VALUES ($1, $2, $3, $4)", record[0], record[1], record[2], record[3])
+		if dbErr != nil {
+			return fmt.Errorf("failed to insert record into database, %v", dbErr)
+		}
+	}
+
+	return nil
+}
 
 func encryptPassword(password string) (string, error) {
 	block, err := aes.NewCipher(secretKey)
@@ -74,6 +192,25 @@ func decryptPassword(encryptedPassword string) (string, error) {
 }
 
 func main() {
+
+	envErr := godotenv.Load("db.env")
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+	var (
+		host     = os.Getenv("DB_HOST")
+		user     = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		dbname   = os.Getenv("DB_NAME")
+	)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, 5432, user, password, dbname)
+	var err error
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var rootCmd = &cobra.Command{Use: "varden"}
 
@@ -126,9 +263,35 @@ func main() {
 		},
 	}
 
-	rootCmd.AddCommand(generateCmd, getCmd, updateCmd, deleteCmd)
+	var dumpDb = &cobra.Command{
+		Use:   "dump [filename]",
+		Short: "Dump Database into zip",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := dumpPasswordsToZip(db, args[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Dumped DB to %v:\n", args[0])
+		},
+	}
 
-	connectDB()
+	var restoreDb = &cobra.Command{
+		Use:   "restore [filename]",
+		Short: "Restore Database from zip",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := restorePasswordsToZip(db, args[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Restored DB from %v:\n", args[0])
+		},
+	}
+
+	rootCmd.AddCommand(generateCmd, getCmd, updateCmd, deleteCmd, dumpDb, restoreDb)
+
+	connectDB(db)
 	defer db.Close()
 
 	if err := rootCmd.Execute(); err != nil {
@@ -137,27 +300,8 @@ func main() {
 	}
 }
 
-func connectDB() {
-	envErr := godotenv.Load("db.env")
-	if envErr != nil {
-		log.Fatal("Error loading .env file")
-	}
-	var (
-		host     = os.Getenv("DB_HOST")
-		user     = os.Getenv("DB_USER")
-		password = os.Getenv("DB_PASSWORD")
-		dbname   = os.Getenv("DB_NAME")
-	)
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, 5432, user, password, dbname)
-	var err error
-	db, err = sql.Open("postgres", psqlInfo)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = db.Ping()
+func connectDB(db *sql.DB) {
+	err := db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
